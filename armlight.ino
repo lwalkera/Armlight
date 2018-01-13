0a924f806084221295050076729904e32f9bfb6e
Laine Walker-Avina <lwalkera@ieee.org>
2018-01-13 15:54:08 -0800

    Add in effects and buzzer

--- armlight.ino
+++ armlight.ino
@@ -42,6 +42,7 @@ SoftwareSerial mySerial(2, 3);  // RX, TX
 LibrePilotSerial lps(&mySerial);
 
 #define NUM_LEDS 5
+#define BUZZER 12
 
 CRGB leds[NUM_LEDS];
 
@@ -54,39 +55,111 @@ void setleds(CRGB color)
   }
   FastLED.show();
 }
+
+void decay_leds()
+{
+  int i;
+  for(i=0; i<NUM_LEDS; i++)
+  {
+    leds[i].fadeToBlackBy(30);
+  }
+  FastLED.show();
+}
+
+void beep(int time)
+{
+	digitalWrite(BUZZER, 1);
+	delay(time);
+	digitalWrite(BUZZER, 0);
+}
+
 void setup() {
+	digitalWrite(BUZZER, 0);
+	digitalWrite(9, 0);
+	pinMode(BUZZER, OUTPUT);
+	pinMode(9, OUTPUT);
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, HIGH);
   //Begin LibrePilotSerial communication
   lps.serial->begin(57600);
-  FastLED.addLeds<APA102, 4, 5, BGR>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
+  FastLED.addLeds<APA102, 4, 5, BGR>(leds, NUM_LEDS);
   setleds(CRGB::White);
+	beep(500);
 }
  
 void loop() {
-  //Request object from FC
-  lps.request(FLIGHTSTATUS_OBJID);
- 
-  //Receive object from FC. This function will block until the specified object was received or it times out.
-  //It returns true if a valid packet was received
-  //the packet is stored in the array of the object packet union
-  boolean ok = lps.receive(FLIGHTSTATUS_OBJID, FlightStatusDataUnion.arr, 200);
- 
-  //the packet data may only be accessed if the return value was true
-  if(ok) {
- 
-    //the packet can be accessed in a structured manner via the data member
-    if (FlightStatusDataUnion.data.Armed == FLIGHTSTATUS_ARMED_ARMED) {
-      //the quad is armed, do something!
-      digitalWrite(LED_BUILTIN, HIGH);
-      setleds(CRGB::Red);
-    }
-    else
+  static unsigned int ticks = 0;
+  static int fs = -1;
+
+  delay(30);
+  if(fs == FLIGHTSTATUS_ARMED_DISARMED)
+  {
+    static int x=0;
+    static int inc = 1;
+
+    decay_leds();
+    if(ticks % 5 == 0)
     {
-      digitalWrite(LED_BUILTIN, LOW);
-      setleds(CRGB::Green);
+      leds[x] = CRGB::Green;
+      x += inc;
+
+      if(x == NUM_LEDS)
+      {
+        x -= 2;
+        inc = -1;
+      }
+      else if(x == 0)
+      {
+        inc = 1;
+      }
     }
   }
- 
-  delay(250);  //wait 250 ms
+  
+  if(ticks++ % 10 == 0)
+  {
+    //Request object from FC
+    lps.request(FLIGHTSTATUS_OBJID);
+
+    //Receive object from FC. This function will block until the specified object was received or it times out.
+    //It returns true if a valid packet was received
+    //the packet is stored in the array of the object packet union
+    boolean ok = lps.receive(FLIGHTSTATUS_OBJID, FlightStatusDataUnion.arr, 200);
+
+    //the packet data may only be accessed if the return value was true
+    if(ok) {
+			int last_fs = fs;
+      fs = FlightStatusDataUnion.data.Armed;
+
+      //the packet can be accessed in a structured manner via the data member
+      switch(FlightStatusDataUnion.data.Armed)
+      {
+        case FLIGHTSTATUS_ARMED_ARMED:
+          //the quad is armed, do something!
+          digitalWrite(LED_BUILTIN, HIGH);
+					if(last_fs != fs)
+					{
+						setleds(CRGB::Red);
+						beep(1000);
+					}
+          break;
+        case FLIGHTSTATUS_ARMED_ARMING:
+					if(last_fs != fs)
+					{
+						setleds(CRGB::Yellow);
+						beep(30);
+					}
+          break;
+        case FLIGHTSTATUS_ARMED_DISARMED:
+          digitalWrite(LED_BUILTIN, LOW);
+					if(last_fs != fs)
+					{
+						setleds(CRGB::Green);
+						beep(100);
+						delay(100);
+						beep(100);
+					}
+          break;
+      }
+    }
+  }
 }
